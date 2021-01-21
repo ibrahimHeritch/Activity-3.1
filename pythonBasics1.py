fe654e1e427490b2db6011581c5d2d840a006ca9
Bob <her.ibrahim@hotmail.com>
2021-01-20 20:27:13 -0500

    updated questions

--- pythonBasics1.py
+++ pythonBasics1.py
@@ -1,33 +1,35 @@
 # Python Activtiy
-# 
-# Fill in the code for the functions below. 
+#
+# Fill in the code for the functions below.
 # The starter code for each function includes a 'return'
 # which is just a placeholder for your code.
 
-# Part A. starts_with
-# Define a function starts_with(s, char) that takes a string and a character
-# and returns true if the string starts with that character and false otherwise. 
-def starts_with(s, char):
+# Part A. count_char
+# Define a function count_char(s, char) that takes a string and a character
+# and returns the number of times char occures in s
+
+def count_char(s, char):
   # YOUR CODE HERE
-  
+
   return
 
-# Part B. starts_with_vowel
-# Define a function starts_with_vowel(s) that takes a string and
-# returns true if the string starts with a vowel and false otherwise. 
-# For our purposes, a consonant is any letter other than A, E, I, O, U)
-# Your solution should work for both upper and lower cases 
-def starts_with_vowel(s):
+# Part B. is_power_of
+# Define a function is_power_of(i,j) that takes 2 ints i and j
+# and checks if i is a power of j or not
+# if there exists an integer x such that i**x=j return True (i is a power of j)
+# otherwise return Flase
+def is_power_of(i,j):
   # YOUR CODE HERE
-  
+
   return
 
-# Part C. max_min_sum
-# Define a function max_min_sum(arr) that takes an array and returns the sum
-# of the largest element and the smallest element of the array.
-# For an empty array it should return zero
-# For an array with just one element, it should return that element
-def max_min_sum(arr):
+# Part C. longest_word
+# Define a function longest_word(s) that takes a string s
+# s is a sentence made up of words separated by a single space " "
+# return the longest word in the sentence
+#
+# in case s in an empty string return an empty string
+def longest_word(s):
   # YOUR CODE HERE
-  
+
   return
